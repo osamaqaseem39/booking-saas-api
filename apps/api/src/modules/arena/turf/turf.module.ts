7b531ba74e7ecbd5df7dfbd54f2044b20a05ede5
osamaqaseem39 <osama.qa.39@gmail.com>
2026-04-15 20:37:21 +0500

    mew

--- apps/api/src/modules/arena/turf/turf.module.ts
+++ apps/api/src/modules/arena/turf/turf.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BusinessesModule } from '../../businesses/businesses.module';
+import { IamModule } from '../../iam/iam.module';
 import { TurfCourt } from './entities/turf-court.entity';
 import { TurfArenaController } from './turf-arena.controller';
 import { TurfController } from './turf.controller';
 import { TurfService } from './turf.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TurfCourt]), BusinessesModule],
+  imports: [TypeOrmModule.forFeature([TurfCourt]), BusinessesModule, IamModule],
   controllers: [TurfController, TurfArenaController],
   providers: [TurfService],
   exports: [TurfService, TypeOrmModule],
